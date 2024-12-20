a0bdff29640c8f9b261e24c3ead3b131ddf85b90
Erick Del Orbe <erick@har.com>
2024-12-20 16:15:06 -0600

    Output inputs to the console

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const github = require("@actions/github");
 
 try {
   console.log(github.context);
+  console.log("needs-review-label", core.getInput("needs-review-label"));
+  console.log("needs-dev-label", core.getInput("needs-dev-label"));
 } catch (error) {
   core.setFailed(error.message);
 }
